bd836528c1fb6478b65d1e217e8b2bd36ae1f397
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-06-29 22:37:38 +0700

    feat(core): applied template

--- lib/utils.sh
+++ lib/utils.sh
@@ -3,3 +3,10 @@
 ## User utility functions
 ## This will load on very beginning of the scripts
 ## so make sure that you only define functions
+
+_kc_asdf_custom_arch() {
+  local os="$KC_ASDF_OS" arch="$1"
+  [[ "$os" == "darwin" ]] &&
+    arch="amd64"
+  printf "%s" "$arch"
+}
